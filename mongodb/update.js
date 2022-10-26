bd9621e662fe97ac8403379048e514e7cb79e0bd
Ranjith101 <89287908+Ranjith101@users.noreply.github.com>
2022-10-26 18:49:46 +0530

    Update update.js

---
+++ mongodb/update.js
@@ -0,0 +1,21 @@
+
+// You can update a record, or document as it is called in MongoDB, by using the updateOne() method.
+
+// The first parameter of the updateOne() method is a query object defining which document to update.
+
+// The second parameter is an object defining the new values of the document.
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var myquery = { address: "Valley 345" };
+  var newvalues = { $set: {name: "Mickey", address: "Canyon 123" } };
+  dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+    if (err) throw err;
+    console.log("1 document updated");
+    db.close();
+  });
+});
